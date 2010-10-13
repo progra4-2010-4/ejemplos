b31993759d45e6fd249c6c457c0198005c7e0489
lfborjas <luis.borjas@escolarea.com>
2010-10-12 23:45:40 -0600

    usando templates, sin layout

--- get_simple/calcu.rb
+++ get_simple/calcu.rb
@@ -9,13 +9,17 @@ get '/calcu' do
     y = params[:y].to_i
     #los arreglos se pasan como arr[]=elem1&arr[]=elem2...
     #p params[:arr]
-    res = case params[:op]
+    #res es una variable de instancia, disponible para el resto de la respuesta
+    @res = case params[:op]
         when "sum" then x + y 
         when "min" then x - y 
         when "div" then x / y
         when "mul" then x * y
     end
-    "El resultado es #{res}"
+
+    #el método erb busca una plantilla llamada como el param y le
+    #pasa las variables de instancia para generar el html
+    erb :resultado
 end
 
    
