d91736a927b5cdb3c121e05862cb0fa901fb965b
lfborjas <luis.borjas@escolarea.com>
2010-10-13 21:47:29 -0600

    listo un form simple

--- get_simple/calcu.rb
+++ get_simple/calcu.rb
@@ -5,8 +5,8 @@ get('/'){ erb :index }
 
 get '/calcu' do
     #Params es un hash que contiene ya sea los parámetros del get o el cuerpo decodificado del post
-    x = params[:x].to_i #vienen como strings, los convertimos a enteros
-    y = params[:y].to_i
+    x = params[:x].to_i || 0 #vienen como strings, los convertimos a enteros
+    y = params[:y].to_i || 0
     #los arreglos se pasan como arr[]=elem1&arr[]=elem2...
     #p params[:arr]
     #res es una variable de instancia, disponible para el resto de la respuesta
@@ -15,6 +15,7 @@ get '/calcu' do
         when "min" then x - y 
         when "div" then x / y
         when "mul" then x * y
+        else "¡Ninguno, porque no has calculado!"
     end
 
     #el método erb busca una plantilla llamada como el param y le
