d95073659585167dce7e1cf131de029fce390c16
lfborjas <luis.borjas@escolarea.com>
2010-10-13 22:11:35 -0600

    listo lo de validar floats y div entre cero

--- get_simple/calcu.rb
+++ get_simple/calcu.rb
@@ -5,15 +5,15 @@ get('/'){ erb :index }
 
 get '/calcu' do
     #Params es un hash que contiene ya sea los parámetros del get o el cuerpo decodificado del post
-    x = params[:x].to_i || 0 #vienen como strings, los convertimos a enteros
-    y = params[:y].to_i || 0
+    x = params[:x].to_f || 0.0 #vienen como strings, los convertimos a enteros
+    y = params[:y].to_f || 0.0
     #los arreglos se pasan como arr[]=elem1&arr[]=elem2...
     #p params[:arr]
     #res es una variable de instancia, disponible para el resto de la respuesta
     @res = case params[:op]
         when "sum" then x + y 
         when "min" then x - y 
-        when "div" then x / y
+        when "div" then (y.zero?) ? "la destrucción del universo" : x / y
         when "mul" then x * y
         else "¡Ninguno, porque no has calculado!"
     end
