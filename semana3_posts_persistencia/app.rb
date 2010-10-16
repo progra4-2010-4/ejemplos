7d2f945c6ba8d5f7ee72e95e5f8dec67784ced34
lfborjas <luis.borjas@escolarea.com>
2010-10-16 17:12:35 -0600

    listo el post sencillo: nada de css ni validaciones

---
+++ semana3_posts_persistencia/app.rb
@@ -0,0 +1,64 @@
+require 'sinatra'
+
+class MusicAlbum 
+    #Esta es una variable de clase
+    @@genres = ["Alternative metal", 
+    "Black metal", 
+    "Death metal", 
+    "Doom metal", 
+    "Extreme metal", 
+    "Gothic metal", 
+    "Industrial metal", 
+    "Neo-classical metal", 
+    "Power metal", 
+    "Progressive metal", 
+    "Speed metal", 
+    "Symphonic metal", 
+    "Technical Death Metal", 
+    "Thrash metal", 
+    "Traditional heavy metal", 
+    "Viking metal",]
+    #el meta-método `attr_accessor` crea un accesor y un mutador para cada
+    #uno de sus parámetros.
+    #p.e., si lo llamásemos con attr_accessor :nombre
+    #sería equivalente  a tener:
+    #def nombre=(nuevo_nombre)
+    # @nombre= nuevo_nombre
+    #end
+    #y
+    #def nombre
+    # return @nombre
+    #end
+    @@max_rating = 10    
+    def self.genres; @@genres; end
+    def self.max_rating; @@max_rating; end
+    #en realidad, sería mejor usar una variable de instancia @genres
+    #y acceder con la metaclase: 
+    #class << self ; attr_reader :genres; end
+
+    attr_accessor :artist, :description, :genre, :rating, :released, :title 
+    
+    #este es el constructor de la clase
+    def initialize(artist, description, genre, rating, released, title)
+       #aquí se inicializan todas las variables de instancia
+       @artist, @description, @genre, @rating, @released, @title = artist, description, genre, rating, released, title 
+    end
+end
+
+
+get '/' do 
+    erb :index
+end
+
+post '/discos' do 
+    #Esta línea de abajo equivale a
+    #@disco = MusicAlbum.new(params[:artist], params[:description], params[:genre]... )
+    #pero no tenía ganas de volver a escribir eso...
+    @album = MusicAlbum.new *params.sort.collect{|e| e[1]}
+    erb :show
+end
+
+get '/discos/new' do 
+    erb :new
+end
+
